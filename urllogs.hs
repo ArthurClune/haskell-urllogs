9431ba16e643ecc57cc73dae326b1465a822e816
Arthur Clune <arthur@clune.org>
2012-05-24 16:08:01 +0100

    acess log parsing started, all tests failing, no code

--- urllogs.hs
+++ urllogs.hs
@@ -1,17 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Codec.Compression.GZip as GZip
-import qualified Data.ByteString.Lazy as BL
+import Data.ByteString.Char8 (readInt)
+import Data.Maybe (fromJust)
 import qualified Data.ByteString.Lazy.Char8 as SL
 import qualified Data.Attoparsec as A
 
-import IpoqueLogs
+import IpoqueLog
 
 -- news.bbcimg.co.uk", url = "/view/2_0_11/cream/hi/shared/components/components.css
 
---matchURL::IpoqueLogLine String -> Bool
-matchURL l s = (sport $ l) == "80"
+getInt    p l = fst. fromJust . readInt $ p l
+getString p l = fst. fromJust $ p l
+
 
 main = do         
-    contents <- fmap GZip.decompress (BL.readFile "/home/arthur/Work/data/url.log.1.gz")
+    contents <- fmap GZip.decompress (SL.readFile "/home/arthur/Work/data/url.log.1.gz")
     mapM_ (print . A.maybeResult . A.parse ipoqueLineParser . toStrict) (SL.lines contents)
