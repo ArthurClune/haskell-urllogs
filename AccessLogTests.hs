679f3160c5ad6274812f7ad2ed48c1a4433c371a
Arthur Clune <arthur@clune.org>
2012-06-02 22:58:49 +0100

    working access log parser

--- AccessLogTests.hs
+++ AccessLogTests.hs
@@ -1,35 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.String
-import Data.ByteString.Char8 (unpack, readInt)
+import qualified Data.Text as T
 import Data.Maybe
 import AccessLog
 import Test.HUnit
-import Data.Attoparsec
+import Data.Attoparsec.Text
 
 
-s = "1330757555.727  43378 144.32.71.165 TCP_MISS/200 418 GET http://0-if-w.channel.facebook.com/pull?channel=p_100003547657244&seq=6&clientid=71560450&cb=f7ry&idle=46041 - DIRECT/69.171.227.51 text/plain"
+s = "1330757555.727  43378 144.32.71.165 TCP_MISS/200 418 GET http://0-if-w.channel.facebook.com/pull?channel=p_100003547657244&seq=6&clientid=71560450&cb=f7ry&idle=46041 - DIRECT/69.171.227.51 text/plain\n"
 urls = "http://0-if-w.channel.facebook.com/pull?channel=p_100003547657244&seq=6&clientid=71560450&cb=f7ry&idle=46041"    
-x = "a line that should not parse"
+x = "a line tha   should not parse" 
 
-getInt s p = fst. fromJust . readInt $ p i
-        where
-            i = fromJust . maybeResult . parse accessLineParser $ fromString s
+myTestCase t a v = doTestCase t a getValue v
+    where
+        getValue s p = p $ (fromJust . maybeResult $ parse accessLineParser s)
+        doTestCase t a f v = TestCase $ assertEqual t a (f s v)
 
-getString s p = p $ (fromJust . maybeResult . parse accessLineParser $ fromString s)
-
-testTS       = TestCase $ assertEqual "TS is 1330757555.727"  "1330757555.727"      (getString s ts)
-testBytes    = TestCase $ assertEqual "Bytes are 43378"       43378                 (getInt s bytes)
-testClientIP = TestCase $ assertEqual "Client IP is 144.32.71.165" "144.32.71.165"  (getString s clientIP)
-testURL      = TestCase $ assertEqual "URL should be right"   urls                  (getString s url)
-testConnType = TestCase $ assertEqual "ConnType is Direct"    "DIRECT"              (getString s connType)
-testServerIP = TestCase $ assertEqual "ServerIP is 69.171.227.51" "69.171.227.51"   (getString s serverIP)
-testMimeType = TestCase $ assertEqual "Mime type is test/html" "test/html"          (getString s mimeType)
+testTS         = myTestCase "TS is 1330757555.727"       1330757555.727  ts
+testElapsed    = myTestCase "Elapsed time is 43378"       43378          elapsed
+testClientIP   = myTestCase "Client IP is 144.32.71.165" "144.32.71.165" clientIP
+testAction     = myTestCase "Action is TCP_MISS/200"     "TCP_MISS/200"  action
+testSize       = myTestCase "Size is 418"                418             size
+testMethod     = myTestCase "Method is GET"              "GET"           method
+testURL        = myTestCase "URL should be right"        urls            url
+testIdent      = myTestCase "Ident should be -"          "-"             ident
+testHierarchy  = myTestCase "Hierarchy is Direct/69.171.227.51" "DIRECT/69.171.227.51" hierarchy
+testMimeType   = myTestCase "Mime type is text/plain"    "text/plain"    mimeType
     
 testFail = TestCase $ assertEqual "Invalid line should fail to parse" Nothing 
-                (maybeResult . parse accessLineParser $ fromString x)
+                (maybeResult $ parse accessLineParser x)
 
-main = runTestTT $ TestList [testTS,       testBytes,    testClientIP,
-                             testURL,      testConnType, testServerIP,
+main = runTestTT $ TestList [testTS,       testElapsed,    testClientIP,
+                             testAction,   testSize,       testMethod,      
+                             testURL,      testIdent,      testHierarchy, 
                              testMimeType, testFail
                             ]
