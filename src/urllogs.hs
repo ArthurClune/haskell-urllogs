43712b1bcc3e3464e0942a17217b46ca9877a763
Arthur Clune <arthur@clune.org>
2012-08-01 16:40:40 +0100

    RankNTypes example

--- src/urllogs.hs
+++ src/urllogs.hs
@@ -1,5 +1,5 @@
 
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RankNTypes #-}
 
 --import Control.Monad
 import System.Environment
@@ -7,16 +7,18 @@ import System.Environment
 import qualified URLAlert.SquidLog as SquidLog
 import qualified URLAlert.IpoqueLog as IpoqueLog
 
-parseFiles :: FilePath -> FilePath -> IO (Int, Int)
-parseFiles f1 f2 = do
-    l1 <- IpoqueLog.parseGZipLog f1::IO [Maybe IpoqueLog.IpoqueLogLine]
-    l2 <- SquidLog.parseGZipLog  f2::IO [Maybe SquidLog.SquidLogLine]
-    let len1 = length l1
-    let len2 = length l2
-    return (len1, len2)
+-- | Apply function f to each parsed "line" from a  file 
+-- given by the parser p, returning the result
+--analyseFile :: forall t (m :: * -> *) b. Monad m => (t -> b) -> m t -> m b
+analyseFile :: forall b c . (forall a . [a] -> b) -> IO [c] -> IO b
+analyseFile f l = do
+    ls <- l
+    let v1 = f ls
+    return v1
 
 main::IO()
 main = do      
     args <- getArgs
-    (len1, len2) <- parseFiles (head args) (last args)
+    len1 <- analyseFile length (IpoqueLog.parseGZipLog $ head args::IO [Maybe IpoqueLog.IpoqueLogLine])
+    len2 <- analyseFile length (SquidLog.parseGZipLog  $ last args::IO [Maybe SquidLog.SquidLogLine])
     print $ "file1 has length " ++ show len1 ++ " and file2 has length " ++ show len2
